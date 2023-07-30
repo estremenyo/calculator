d787c39efc7eb73a7e74649c6c5f1ec0e0659be7
Estremenyo <109721927+estremenyo@users.noreply.github.com>
2023-07-30 13:41:27 +0400

    Add function placeholders

---
+++ script.js
@@ -0,0 +1,21 @@
+let num1, operator, num2;
+
+function add(num1, num2) {
+
+}
+
+function subtract(num1, num2) {
+
+}
+
+function multiply(num1, num2) {
+
+}
+
+function divide(num1, num2) {
+
+}
+
+function operate(num1, operator, num2) {
+
+}
