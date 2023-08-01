c86284035df5317602118a623358ef79e554394e
Estremenyo <109721927+estremenyo@users.noreply.github.com>
2023-08-01 14:11:54 +0400

    Add clear button functionality

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let num1, operator, num2;
+let display = document.querySelector(".display");
+
+document.querySelector(".clear").addEventListener("click", removeFromDisplay);
+
+let numberButtons = document.querySelectorAll(".number");
+let operatorButtons = document.querySelectorAll(".operator");
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -22,3 +29,9 @@ function operate(num1, operator, num2) {
     if (operator == "*") return multiply(num1, num2);
     if (operator == "/") return divide(num1, num2);
 }
+
+
+function removeFromDisplay(e) {
+    display.textContent = "";
+}
+
