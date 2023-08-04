2d05b8af85bedb22b03123063e6c55d3e1f7dcbd
Estremenyo <109721927+estremenyo@users.noreply.github.com>
2023-08-04 11:40:41 +0400

    Add alpha addition functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let display = document.querySelector(".display");
 let showingHello = true;
 
 document.querySelectorAll("button").forEach(button => 
-    button.addEventListener("click", clearInitial, {once: true}));
+    button.addEventListener("click", clearInitial));
 
 document.querySelector(".clear").addEventListener("click", removeFromDisplay);
 
@@ -33,6 +33,8 @@ function divide(num1, num2) {
 }
 
 function operate(num1, operator, num2) {
+    num1 = +num1;
+    num2 = +num2;
     if (operator == "+") return add(num1, num2);
     if (operator == "-") return subtract(num1, num2);
     if (operator == "*") return multiply(num1, num2);
@@ -60,5 +62,28 @@ function updateDisplay(e) {
 }
 
 function evaluateDisplay() {
-    console.log("yeah");
+    let recordingSecond = false;
+    // Loop over the user input
+    for (i = 0; i < display.textContent.length; i++) {
+        if (checkNum(display.textContent[i]) && recordingSecond == false) {
+            num1 += display.textContent[i];
+        }
+    
+    }
+    num3 = operate(num1, "+", 2);
+    console.log(num1);
+    console.log(num3);
+    display.textContent = num3;
+
+    num1 = "";
+    operator = "";
+    num2 = "";
 }
+
+    function checkNum(char) {
+        // If a number or decimal, return true
+        if (char >= 0 || char == ".") {
+            return true;
+        } // else is operator, return false
+        else return false;
+    }
