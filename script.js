f877171060080b413e93e070fdb283be60834267
Estremenyo <109721927+estremenyo@users.noreply.github.com>
2023-08-04 16:43:26 +0400

    Fixed divided by zero functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let num1 = "";
 let operator = "";
 let num2 = "";
 let display = document.querySelector(".display");
-let showingHello = true;
+let needToClearScreen = true;
 let decimalExists = false;
 
 document.querySelectorAll("button").forEach(button => 
@@ -44,18 +44,17 @@ function operate(num1, operator, num2) {
 
 
 function removeFromDisplay(e) {
-        display.textContent = "";
-        num1 = "";
-        num2 = "";
-        operator = "";
-        decimalExists = false;
-
+    display.textContent = "";
+    num1 = "";
+    num2 = "";
+    operator = "";
+    decimalExists = false;
 }
 
 function clearInitial() {
-    if (showingHello == true) {
+    if (needToClearScreen == true) {
     display.textContent = "";
-    showingHello = false;
+    needToClearScreen = false;
     num1 = "";
     num2 = "";
     operator = "";
@@ -74,67 +73,42 @@ function updateDisplay(e) {
 
 function evaluateDisplay() {
     let recordingSecond = false;
-    // Loop over the user input
-    for (i = 0; i < display.textContent.length; i++) {
-        // Record the first number
-        if (checkNum(display.textContent[i]) && recordingSecond == false) {
+    for (let i = 0; i < display.textContent.length; i++) {
+        if (isNum(display.textContent[i]) && recordingSecond == false) {
             num1 += display.textContent[i];
-        // Record the operator
-        } else if (!checkNum(display.textContent[i]) && recordingSecond == false) {
+        } else if (!isNum(display.textContent[i]) && recordingSecond == false) {
             operator = display.textContent[i];
             recordingSecond = true;
-        // Record the second number
-        } else if (checkNum(display.textContent[i]) && recordingSecond == true) {
+        } else if (isNum(display.textContent[i]) && recordingSecond == true) {
             num2 += display.textContent[i];
         }
-        // If we are already recording the second number but see another operator,
-        // we need to operate on our current numbers before moving on to the next
-        else if (!checkNum(display.textContent[i]) && recordingSecond == true) {
-            // TODO
+        else if (!isNum(display.textContent[i]) && recordingSecond == true) {
             num1 = operate(num1, operator, num2);
             num2 = "";
             operator = display.textContent[i];
-        }
-        // Check if the user is dividing by zero
-        // TODO: If num2 is '0' w/o a fraction, run the conditional
-        // But if num2 is 0 w/ a fraction e.g. '0.7', don't run it
-        if (operator == "/" && num2 == "0") {
-            display.textContent = "ERROR!";
-            showingHello = true;
-            return;
-        }    
+        } 
     }
-    // If the user is trying to divide or multiply by nothing, set it to 1
-    if ((operator == "/" || operator == "*") && (num2 == "" || num2 == ".")) {
-        num2 = 1;
-    }    
 
-    // Operate on our variables the only or last time
     num1 = operate(num1, operator, num2);
-
-    // Round decimals to 5 places if the output has a decimal longer than that
-    num1 = checkDecimal(num1);
-    display.textContent = num1;
-    showingHello = true;
+    num1 = checkDecimal(num1, 5);
+    if (num1 == Infinity) display.textContent = "ERROR";
+    else display.textContent = num1;
+    needToClearScreen = true;
 }
 
 
-function checkNum(char) {
-    // If a number or decimal point, return true
-    if (char >= 0 || char == ".") {
-        return true;
-    } // else is operator, return false
+function isNum(char) {
+    if (char >= 0 || char == ".") return true;
     else return false;
 }
 
-function checkDecimal(num) {
+function checkDecimal(num, n) {
     index = num.toString().indexOf(".");
     if (index == -1) return num;
     else {
         decimalPart = num.toString().substring(index + 1);
-        if (decimalPart.length > 5) {
-            return num.toFixed(5)
+        if (decimalPart.length > n) {
+            return num.toFixed(n)
         } else return num;
-
     }
 }
